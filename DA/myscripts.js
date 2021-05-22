d8e241eb50a19fcec9725f3175849f404d7d6a98
Project2021228 <81644697+Project2021228@users.noreply.github.com>
2021-05-22 21:55:25 +0500

    Klimu

---
+++ DA/myscripts.js
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="pochemu)v.1.css">
+<!--Import Google Icon Font-->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<!--Import materialize.css-->
+<link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection"/>
+<meta charset="utf-8">
+
+<!--Let browser know website is optimized for mobile-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+<!DOCTYPE html>
+<html>
+<head>
+<!--Import Google Icon Font-->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<!--Import materialize.css-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/cs..">
+
+
+<!--Let browser know website is optimized for mobile-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+<nav>
+<div class="container">
+<div class="nav-wrapper">
+<a href="#" class="brand-logo">Logo</a>
+<ul id="nav-mobile" class="right hide-on-med-and-down">
+<li><a href="1.html">Задание 1</a></li>
+<li><a href="2.html">Задание 16</a></li>
+</ul>
+</div>
+</div>
+</nav>
+
+<!— Modal Trigger —>
+<a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
+
+<!— Modal Structure —>
+<div id="modal1" class="modal">
+<div class="modal-content">
+<i class="material-icons">airline_seat_flat_angled </i><h4>Справочный материал</h4>
+<p><b><h3>Здесь находятся таблицы переводов в различные системы счисления</h3></b></p>
+<p class="center">
+Перевод из двоичной системы счисления в восмеричную систему счисления.
+</p>
+<div class="container">
+<table border="1">
+<tr>
+<th>Ячейка 1</th>
+<th>Ячейка 2</th>
+</tr>
+<tr>
+<td>Ячейка 3</td>
+<td>Ячейка 4</td>
+</tr>
+</table>
+</div>
+</div>
+<div class="modal-footer">
+<a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+</div>
+</div>
+
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js.."></script>
+<script type="text/javascript">
+$(document).ready(function(){
+$('.modal').modal();
+});
+</script>
+</body>
+</html
